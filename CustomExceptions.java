10fcafd35ee92c38ddd96714a38529dde3fa0050
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-04-18 20:24:09 +0530

    exceptions

--- CustomExceptions.java
+++ CustomExceptions.java
@@ -1,5 +1,16 @@
-package javalab.threading;
+// CustomExceptions.java
+// This file contains two custom exceptions that can be used across thread classes
 
-public class CustomExceptions {
-    
+// Custom exception for when thread data is invalid
+class InvalidThreadDataException extends Exception {
+    public InvalidThreadDataException(String message) {
+        super(message);  // Pass custom error message to Exception class
+    }
+}
+
+// Custom exception for thread timeout scenarios
+class ThreadTimeoutException extends Exception {
+    public ThreadTimeoutException(String message) {
+        super(message);
+    }
 }
