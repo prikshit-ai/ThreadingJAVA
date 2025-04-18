08711cace48fec13e78d60dfb7674889e850a4ec
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-04-18 20:19:32 +0530

    printAlphabets

--- PrintAlphabets.java
+++ PrintAlphabets.java
@@ -1,5 +1,16 @@
-package javalab.threading;
+public class PrintAlphabets implements Runnable {
 
-public class PrintAlphabets {
-    
+    @Override
+    public void run() {
+        try {
+            for (char ch = 'A'; ch <= 'J'; ch++) {
+                System.out.println("Alphabet: " + ch);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("PrintAlphabets thread interrupted.");
+        } catch (Exception e) {
+            System.out.println("Unexpected error in PrintAlphabets: " + e.getMessage());
+        }
+    }
 }
