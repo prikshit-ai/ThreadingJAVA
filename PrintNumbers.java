4c01a773ff752770c426987bfb301941cf4b9014
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-04-18 20:19:06 +0530

    printNumbers

--- PrintNumbers.java
+++ PrintNumbers.java
@@ -1,5 +1,16 @@
-package javalab.threading;
+public class PrintNumbers implements Runnable {
 
-public class PrintNumbers {
-    
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("Number: " + i);
+                Thread.sleep(500); // Pause for half a second
+            }
+        } catch (InterruptedException e) {
+            System.out.println("PrintNumbers thread interrupted.");
+        } catch (Exception e) {
+            System.out.println("Unexpected error in PrintNumbers: " + e.getMessage());
+        }
+    }
 }
